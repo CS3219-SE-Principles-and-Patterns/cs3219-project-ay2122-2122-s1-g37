42af445a70b55f9095b276f93bb8f1a88eebb273
Tomashiwa <wongqinliang@gmail.com>
2021-10-29 21:03:08 +0800

    Tidy up room.styled.js

--- client/src/pages/Room/Room.styled.js
+++ client/src/pages/Room/Room.styled.js
@@ -14,7 +14,9 @@ export const RoomContainerWrapper = styled.div`
 	width: 98%;
 
 	display: grid;
+	grid-template-rows: none;
 	grid-template-columns: 1fr auto;
+	grid-row-gap: 0em;
 	grid-column-gap: 1em;
 	align-items: center;
 	justify-content: space-evenly;
@@ -47,61 +49,8 @@ export const RoomContainerWrapper = styled.div`
 		grid-template-rows: auto 4fr 8fr auto;
 		grid-row-gap: 1em;
 	}
-`;
 
-const RoomPageWrapper2 = styled.div`
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	justify-content: space-around;
-
-	background: ${(props) => props.theme.lightGray};
-	height: 100%;
-	max-height: 100%;
-
-	.room-player {
-		height: 80%;
-		width: 72.5%;
-
-		.room-join-fallback {
-			height: 100%;
-			width: 100%;
-
-			display: flex;
-			flex-direction: column;
-			align-items: center;
-			justify-content: center;
-		}
-
-		.room-res-wrapper {
-			position: relative;
-			padding-top: 56.25%;
-			.react-player {
-				position: absolute;
-				top: 0;
-				left: 0;
-			}
-		}
-	}
-	.room-sidebar {
-		height: 80%;
-		width: 22.5%;
-
-		display: flex;
-		flex-direction: column;
-		justify-content: space-around;
-
-		.watchmates {
-			height: 25%;
-		}
-
-		.chatbox {
-			height: 50%;
-		}
-
-		.sidebar-btn-container {
-			display: grid;
-			grid-template-columns: 3fr 1fr;
-		}
+	@media (max-width: 1000px) {
+		display: block;
 	}
 `;
